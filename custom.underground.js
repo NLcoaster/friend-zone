30dbe3b5565c3b065d9d61ee76c1bae2df9ced7c
Gekko90 <91679008+Gekko90@users.noreply.github.com>
2026-03-15 20:48:16 +0100

    underground layer
    bleek dat hier de spawn point ook nog entry in stond

--- custom.underground.js
+++ custom.underground.js
@@ -39,21 +39,7 @@ UnminedCustomUnderground = {
     isEnabled: true,
 
     underground: [
-        //spawn
-        {
-            x: 0,
-            z: 0,
-            image: "compass.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "World Spawn",
-            textColor: "red",
-            offsetX: 0,
-            offsetY: 20,
-            font: "bold 20px Calibri,sans serif",
-        },
-            
-        //player build locations
+     //player build locations
             //hotel
             {
                 x: -51,
